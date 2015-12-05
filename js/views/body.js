fd986b5ebf693e58afe70d5ba5dfe2cc8d0281bc
Hosoyama Terunobu <hosoyama@mediba.jp>
2015-12-06 07:13:23 +0900

    試しにUA判定

--- js/views/body.js
+++ js/views/body.js
@@ -7,8 +7,9 @@ define([
     'jquery',
     'underscore',
     'backbone',
+    'models/ua',
     'hbars!templates/body'
-], function($, _, Backbone, template) {
+], function($, _, Backbone, ua, template) {
     'use strict';
 
     var BodyView = Backbone.View.extend({
@@ -19,6 +20,10 @@ define([
         },
         render: function() {
             var _this = this;
+
+            // デバイスの判定を楽にしたい
+            console.log(ua.iOS ? 'iOS' : 'not iOS');
+
             this.$el.hide().html(template).fadeIn('normal', function() {
                 Backbone.mediator.on('resize', _this.onResize, this);
                 _this.finalize();
